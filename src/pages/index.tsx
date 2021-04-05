0a217795d1461ed89dc64b7aa48574588577ee50
Filipe Kohls <filiperenank@gmail.com>
2021-04-05 20:25:36 -0300

    Integração com a API stripe, para testes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+// Client-side (quando não precisa indexação, quando é uma informação carregada a partir de uma ação do usuário...)
+// Server-side rendering (quando precisa indexação do google e precisa de dados dinamicos como os dados do usuário que está acessando)
+// Static Site Generation (para casos que consegue gerar o html de uma página para que todas as pessoas vejam a mesma coisa, e que precisa de indexação do google)
+
+//Exemplo: Post no blog
+// -> Conteúdo (SSG)
+// -> Comentários do post (Client-side)
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,13 +32,33 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+//SSG (Static Site Generation)
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Id18uK1YUXKRUk6AU3ROptI');
+  
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
